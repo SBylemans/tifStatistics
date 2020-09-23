1ec10755b72a259471f9d0f3748464d52523ec68
sbylemans <sander.bylemans@continuum.be>
2020-09-23 20:49:04 +0200

    Wit percentage

--- hist.py
+++ hist.py
@@ -26,27 +26,29 @@ def __histFile(f):
         img = cv2.imread(f)
         hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-        # white = np.where(img[:,:,1] <= 1 and img[:,:,2] >= 70)
-        # print(white)
-        # print(img)
         reshaped = hsv.reshape(-1, hsv.shape[-1])
 
         del hsv
         del img
 
-        # Get the unique indices and their counts
+        # Get the unique color indices and their counts
         _, unq_idx, counts = np.unique(reshaped, return_index = True, return_counts=True, axis=0)
-        while len(counts) > 0:
-            # print((reshaped[unq_idx[np.argmax(counts)]][1]/255)*100, (reshaped[unq_idx[np.argmax(counts)]][2]/255)*100)
-            if (reshaped[unq_idx[np.argmax(counts)]][1]/255)*100 <= 5 and (reshaped[unq_idx[np.argmax(counts)]][2]/255)*100 >= 50:
-                break
-            else:
-                unq_idx = np.delete(unq_idx, np.argmax(counts))
-                counts = np.delete(counts, np.argmax(counts))
-        if len(counts) > 0:
-            return [f, cv2.cvtColor(np.uint8([[reshaped[unq_idx[np.argmax(counts)]]]]), cv2.COLOR_HSV2RGB)[0,0]]
 
-    return [f, None, None]
+        condition = np.where((reshaped[unq_idx][:,1].astype(np.int32) * reshaped[unq_idx][:,2].astype(np.int32)) > 2500)
+
+        return [f, __getWhiteBand(reshaped, counts, unq_idx), np.sum(counts[condition[0]])/np.sum(counts)]
+
+def __getWhiteBand(img, count_array, idx_array):
+    while len(count_array) > 0:
+        if (img[idx_array[np.argmax(count_array)]][1]/255)*100 <= 5 and (img[idx_array[np.argmax(count_array)]][2]/255)*100 >= 50:
+            break
+        else:
+            idx_array = np.delete(idx_array, np.argmax(count_array))
+            count_array = np.delete(count_array, np.argmax(count_array))
+    if len(count_array) > 0:
+        return cv2.cvtColor(np.uint8([[img[idx_array[np.argmax(count_array)]]]]), cv2.COLOR_HSV2RGB)[0,0]
+
+    return None
 
 # print(__histFile(sys.argv[1]))
 
